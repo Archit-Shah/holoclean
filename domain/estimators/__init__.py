658202d60653da20e95a57defca079717bc0e7f8
Richard Wu <richardwu1997@gmail.com>
2019-03-12 14:29:27 -0400

    Removed EmbeddingRegression references.

--- domain/estimators/__init__.py
+++ domain/estimators/__init__.py
@@ -1,7 +1,6 @@
 from .logistic import Logistic
 from .naive_bayes import NaiveBayes
-from .embedding_regression import EmbeddingRegression
 from .tuple_embedding import TupleEmbedding
 
 
-__all__ = ['Logistic', 'NaiveBayes', 'EmbeddingRegression', 'TupleEmbedding']
+__all__ = ['Logistic', 'NaiveBayes', 'TupleEmbedding']
